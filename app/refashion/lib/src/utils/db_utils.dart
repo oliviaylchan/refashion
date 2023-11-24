44122ce6714229f2a8ec1cb5cd208725b67226e1
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-11-23 23:02:02 -0500

    mongodb
    added the photobutton state handler

---
+++ app/refashion/lib/src/utils/db_utils.dart
@@ -0,0 +1,43 @@
+import 'dart:ffi';
+
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// Function to connect to the MongoDB database
+Future<Db> connectToDatabase() async {
+  await dotenv.load(fileName: '.env');
+
+  if (dotenv.env['DB_CONNECTION_STRING'] == null) {
+    throw Exception('DB_CONNECTION_STRING not found in .env file');
+  }
+
+  final db = await Db.create(dotenv.env['DB_CONNECTION_STRING']!);
+  await db.open();
+  return db;
+}
+
+// Function to pull data from a collection in the database
+Future<List<Map<String, dynamic>>> pullData(String collectionName) async {
+  final db = await connectToDatabase();
+  final collection = db.collection(collectionName);
+  final data = await collection.find().toList();
+  await db.close();
+  return data;
+}
+
+// Function to push data to a collection in the database
+Future<void> pushData(String collectionName, Map<String, dynamic> data) async {
+  final db = await connectToDatabase();
+  final collection = db.collection(collectionName);
+  await collection.insert(data);
+  await db.close();
+}
+
+// Function to update data in a collection in the database
+Future<void> updatePhotoButtonState(bool value) async {
+  final db = await connectToDatabase();
+  final collection = db.collection("state");
+  await collection.update(
+      where.eq('name', 'photoButton'), modify.set('value', value));
+  await db.close();
+}
