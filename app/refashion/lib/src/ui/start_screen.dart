14f91fbc81eb1a8085e3b512d8c014d0c5770232
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-12-02 19:08:27 -0500

    update start screen

--- app/refashion/lib/src/ui/start_screen.dart
+++ app/refashion/lib/src/ui/start_screen.dart
@@ -34,7 +34,7 @@ class _StartScreenState extends State<StartScreen> {
                         'refashion',
                         style: TextStyle(
                           fontFamily: 'Chopper',
-                          fontSize: 55,
+                          fontSize: 80,
                           color: Color.fromARGB(255, 236, 236, 236),
                           shadows: [
                             Shadow(
@@ -70,31 +70,33 @@ class _StartScreenState extends State<StartScreen> {
     );
   }
 
- Widget addButton() {
-  return Align(
-    alignment: Alignment.bottomCenter,
-    child: Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: ElevatedButton(
-        onPressed: () async {
-            Navigator.pushReplacement(context, FadeTransitionTo(screen: const MainNavigation()));
-            SharedPreferences preferences = await SharedPreferences.getInstance();
-             preferences.setBool("loggedIn", true);
+  Widget addButton() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: ElevatedButton(
+          onPressed: () async {
+            Navigator.pushReplacement(
+                context, FadeTransitionTo(screen: const MainNavigation()));
+            SharedPreferences preferences =
+                await SharedPreferences.getInstance();
+            preferences.setBool("loggedIn", true);
           },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          side: const BorderSide(
-              width: 0.5, color: Color.fromARGB(255, 236, 236, 236)),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            side: const BorderSide(
+                width: 0.5, color: Color.fromARGB(255, 236, 236, 236)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
-        ),
-        child: const Text(
-          'continue',
-          style: TextStyle(
-              fontFamily: 'Ubuntu Mono',
-              fontSize: 16,
-              color: Color.fromARGB(255, 236, 236, 236)),
+          child: const Text(
+            'continue',
+            style: TextStyle(
+                fontFamily: 'Ubuntu Mono',
+                fontSize: 16,
+                color: Color.fromARGB(255, 236, 236, 236)),
           ),
         ),
       ),
@@ -125,18 +127,18 @@ class ForegroundPainter extends CustomPainter {
     const p11 = Offset(28, 120);
     const p12 = Offset(20, 140);
 
-    const p13 = Offset(370+20, 765+110);
-    const p14 = Offset(354+20, 765+110);
-    const p15 = Offset(346+20, 765+110);
-    const p16 = Offset(270+20, 765+110);
-    const p17 = Offset(362+20, 757+110);
-    const p18 = Offset(290+20, 757+110);
-    const p19 = Offset(370+20, 749+110);
-    const p20 = Offset(346+20, 749+110);
-    const p21 = Offset(370+20, 741+110);
-    const p22 = Offset(354+20, 741+110);
-    const p23 = Offset(362+20, 692+110);
-    const p24 = Offset(370+20, 665+110);
+    const p13 = Offset(370 + 355, 765 + 365);
+    const p14 = Offset(354 + 355, 765 + 365);
+    const p15 = Offset(346 + 355, 765 + 365);
+    const p16 = Offset(270 + 355, 765 + 365);
+    const p17 = Offset(362 + 355, 757 + 365);
+    const p18 = Offset(290 + 355, 757 + 365);
+    const p19 = Offset(370 + 355, 749 + 365);
+    const p20 = Offset(346 + 355, 749 + 365);
+    const p21 = Offset(370 + 355, 741 + 365);
+    const p22 = Offset(354 + 355, 741 + 365);
+    const p23 = Offset(362 + 355, 692 + 365);
+    const p24 = Offset(370 + 355, 665 + 365);
 
     //Top left corner
     canvas.drawLine(p1, p2, paint);
@@ -163,21 +165,33 @@ class ForegroundPainter extends CustomPainter {
     canvas.drawLine(p21, p24, paint);
 
     //Hanger logo
-    const h1 = Offset(150+10, 480+50); const h2 = Offset(240+10, 480+50);
-    const h3 = Offset(200+10, 448+50); const h4 = Offset(150+10, 473+50);
-    const h5 = Offset(220+10, 473+50); const h6 = Offset(220+10, 445+50);
-    const h7 = Offset(195+10, 425+50); const h8 = Offset(175+10, 435+50);
-    const h9 = Offset(180+10, 440+50);
-    canvas.drawLine(h1.translate(2,2), h2.translate(2,2), paintS); canvas.drawLine(h3.translate(2,2), h2.translate(2,2), paintS);
-    canvas.drawLine(h4.translate(2,2), h3.translate(2,2), paintS); canvas.drawLine(h4.translate(2,2), h5.translate(2,2), paintS);
-    canvas.drawLine(h1.translate(2,2), h6.translate(2,2), paintS); canvas.drawLine(h6.translate(2,2), h7.translate(2,2), paintS);
-    canvas.drawLine(h7.translate(2,2), h8.translate(2,2), paintS); canvas.drawLine(h8.translate(2,2), h9.translate(2,2), paintS);
-    canvas.drawLine(h9.translate(2,2), h7.translate(2,2), paintS);
+    const h1 = Offset(150 + 180, 480 + 200);
+    const h2 = Offset(240 + 180, 480 + 200);
+    const h3 = Offset(200 + 180, 448 + 200);
+    const h4 = Offset(150 + 180, 473 + 200);
+    const h5 = Offset(220 + 180, 473 + 200);
+    const h6 = Offset(220 + 180, 445 + 200);
+    const h7 = Offset(195 + 180, 425 + 200);
+    const h8 = Offset(175 + 180, 435 + 200);
+    const h9 = Offset(180 + 180, 440 + 200);
+    canvas.drawLine(h1.translate(2, 2), h2.translate(2, 2), paintS);
+    canvas.drawLine(h3.translate(2, 2), h2.translate(2, 2), paintS);
+    canvas.drawLine(h4.translate(2, 2), h3.translate(2, 2), paintS);
+    canvas.drawLine(h4.translate(2, 2), h5.translate(2, 2), paintS);
+    canvas.drawLine(h1.translate(2, 2), h6.translate(2, 2), paintS);
+    canvas.drawLine(h6.translate(2, 2), h7.translate(2, 2), paintS);
+    canvas.drawLine(h7.translate(2, 2), h8.translate(2, 2), paintS);
+    canvas.drawLine(h8.translate(2, 2), h9.translate(2, 2), paintS);
+    canvas.drawLine(h9.translate(2, 2), h7.translate(2, 2), paintS);
 
-    canvas.drawLine(h1, h2, paint); canvas.drawLine(h3, h2, paint);
-    canvas.drawLine(h4, h3, paint); canvas.drawLine(h4, h5, paint);
-    canvas.drawLine(h1, h6, paint); canvas.drawLine(h6, h7, paint);
-    canvas.drawLine(h7, h8, paint); canvas.drawLine(h8, h9, paint);
+    canvas.drawLine(h1, h2, paint);
+    canvas.drawLine(h3, h2, paint);
+    canvas.drawLine(h4, h3, paint);
+    canvas.drawLine(h4, h5, paint);
+    canvas.drawLine(h1, h6, paint);
+    canvas.drawLine(h6, h7, paint);
+    canvas.drawLine(h7, h8, paint);
+    canvas.drawLine(h8, h9, paint);
     canvas.drawLine(h9, h7, paint);
   }
 
